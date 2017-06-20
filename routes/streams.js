355b367a65cdc9df2cb9fc856dee92a2cd306f44
Ruben Rutten <ruben@rubenrutten.nl>
2017-06-20 17:50:49 +0200

    Fixed creation of streams

--- routes/streams.js
+++ routes/streams.js
@@ -33,6 +33,7 @@ router.post("/", (req, res) => {
 		return res.status(response.status).sign(response.data);
 	})
 	.catch((err) => {
+		console.log(err);
 		res.status(500).sign({
 			success: false,
 			error: {
@@ -73,86 +74,9 @@ router.put("/:streamId", (req, res) => {
 });
 
 router.delete("/:streamId", (req, res) => {
-	if(typeof req.params.streamId != "string" || /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(req.params.streamId) == false) {
-		return res.status(400).sign({
-			success: false,
-			error: {
-				code: "invalidParameter"
-			}
-		});
-	}
-
-	streams.get(req.params.streamId)
-	.then((stream) => {
-		if(!stream) {
-			return res.status(404).sign({
-				success: false,
-				error: {
-					code: "resourceNotFound"
-				}
-			});	
-		}
-
-		if(stream.ownerId != req.user.id) {
-			return res.status(403).sign({
-				success: false,
-				error: {
-					code: "streams#notStreamOwner"
-				}
-			});
-		}
-
-		return discovery.send("stop", {
-			streamId: stream.id,
-			chatServer: {
-				type: "chat",
-				id: stream.chatServer.id
-			},
-			streamingServer: {
-				type: "streaming",
-				id: stream.streamingServer.id
-			},
-			reason: "manuallyStopped"
-		}, true, 10000)
-		.then((response) => {
-			if(response.success) {
-				var satoshi = stream.satoshi;
-				if(response.streamingServer && response.streamingServer.satoshi) {
-					var satoshiResponse = parseInt(response.streamingServer.satoshi);
-					if(!isNaN(satoshiResponse)) {
-						satoshi = satoshiResponse;
-					}
-				}
-
-				return users.updateSatoshi(req.user.id, satoshi)
-				.then(() => {
-					return streams.remove(stream.id)
-				})
-				.then(() => {
-					return res.status(200).sign({
-						success: true,
-						chatServer: response.chatServer,
-						streamingServer: response.streamingServer
-					});
-				});
-			}
-
-			return res.status(500).sign({
-				success: false,
-				error: {
-					code: response.error.code
-				}
-			});
-		})
-		.catch((err) => {
-			console.error("[Discovery/Stop]", err);
-			res.status(500).sign({
-				success: false,
-				error: {
-					code: "unknownError"
-				}
-			})
-		});
+	streams.remove(req.params.streamId, req.user)
+	.then((response) => {
+		res.status(response.status).sign(response.data);
 	})
 	.catch((err) => {
 		res.status(500).sign({
